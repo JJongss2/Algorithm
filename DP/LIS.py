a1f6bc34c83582d15ad0270cda9de98eaa78fd6f
JongHyun Kim <101381470+JJongss2@users.noreply.github.com>
2022-11-29 14:07:49 +0900

    Update LIS.py

--- DP/LIS.py
+++ DP/LIS.py
@@ -9,3 +9,18 @@ def LIS_DP(seq):
 seq = input()
 lis = LIS_DP(seq)
 print(lis)
+
+'''
+def LIS_DP(seq):
+	LIS = [1]*len(seq)
+	for i in range(1,len(seq)):
+		LIS[i] = 1
+		for j in range(i-1, -1 ,-1):
+			if seq[j] < seq[i]:
+				LIS[i] = max(LIS[i], LIS[j]+1)
+	return max(LIS)
+
+seq = input()
+lis = LIS_DP(seq)
+print(lis)
+'''
